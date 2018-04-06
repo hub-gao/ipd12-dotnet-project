3df061771f0e497eca3636c6c245fb5af22d77a2
DESKTOP-3V910UQ\gao32 <gao32767@gmail.com>
2018-04-05 23:29:51 -0400

    * day 9 update

---
+++ Ipd12_dotnet_project/Assets/Scripts/BlockNonDestroy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class BlockNonDestroy : MonoBehaviour
+{
+    Vector3 XPos;
+    Vector3 YPos;
+    Vector3 ZPos;
+
+    private void Start()
+    {
+        XPos = transform.position;
+        YPos = transform.position;
+        ZPos = transform.position;
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        // disable trigger
+
+    }
+}
