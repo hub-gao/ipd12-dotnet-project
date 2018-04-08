d18171ffbe8f89a98178af6b5b12ab6f14aea3ad
aneetaa <aneetabox@gmail.com>
2018-04-08 19:49:40 -0400

    * Updated

---
+++ Ipd12_dotnet_project/Assets/Scripts/GoToLevel2.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoToLevel2 : MonoBehaviour {
+
+    public GameObject LastCoin;
+
+    void start()
+    {
+        LastCoin.SetActive(true);
+    }
+
+    void OnTriggerEnter()
+    {
+        LastCoin.SetActive(false);
+        Application.LoadLevel("Level-02");
+    }
+}
